ba37ded3b921b45aed66051f1f03d247f99c8fbc
ANDERSON LUIZ SANCHES CARLUCCI PEREIRA <andy2903.alp@gmail.com>
2021-06-15 17:04:11 -0300

    Update product.go
    Olá gente tudo bem com vocês ?
    Gente no modelo antigo estava duplicando os produtos, pois não estavam alterando!
    Eu enxuguei um pouco os testes pois haviam if`s que não faziam sentido! Estavam prejudicando a cobertura de código.
    Desde já agradeço e peço desculpas pelo incomodo.

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -10,17 +10,14 @@ type ProductDb struct {
 	db *sql.DB
 }
 
-func NewProductDb(db *sql.DB) *ProductDb {
+func NewProductDb(db *sql.DB) *ProductDb{
 	return &ProductDb{db: db}
 }
 
-func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+func (p *ProductDb) Get(id string) (application.ProductInterface, error)  {
 	var product application.Product
-	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
-	if err != nil {
-		return nil, err
-	}
-	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	stmt, _ := p.db.Prepare("select id, name, price, status from products where id=?")
+	err := stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
 	if err != nil {
 		return nil, err
 	}
@@ -28,9 +25,8 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 }
 
 func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
-	var rows int
-	p.db.QueryRow("Select id from products where id=?", product.GetID()).Scan(&rows)
-	if rows == 0 {
+	productData, _ := p.Get(product.GetID())
+	if productData == nil {
 		_, err := p.create(product)
 		if err != nil {
 			return nil, err
@@ -46,9 +42,6 @@ func (p *ProductDb) Save(product application.ProductInterface) (application.Prod
 
 func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
 	stmt, err := p.db.Prepare(`insert into products(id, name, price, status) values(?,?,?,?)`)
-	if err != nil {
-		return nil, err
-	}
 	_, err = stmt.Exec(
 		product.GetID(),
 		product.GetName(),
@@ -58,10 +51,8 @@ func (p *ProductDb) create(product application.ProductInterface) (application.Pr
 	if err != nil {
 		return nil, err
 	}
-	err = stmt.Close()
-	if err != nil {
-		return nil, err
-	}
+	defer stmt.Close()
+
 	return product, nil
 }
 
